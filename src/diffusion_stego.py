4750508952cf3eae27516b83c2b338bed7f3cd46
Alcybr <aslahaddin@gmail.com>
2022-07-12 20:54:18 -0500

    stego: ECC headers + bit packing for kilobit payloads

--- src/diffusion_stego.py
+++ src/diffusion_stego.py
@@ -25,3 +25,23 @@ def message_to_seed(msg: str) -> int:
     return int.from_bytes(h[:4], 'big')  # 32-bit seed
 
 
+def generate(message: str, out_path: str, model_id='runwayml/stable-diffusion-v1-5'):
+    seed = message_to_seed(message)
+    print(f'message -> seed: {seed}')
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    pipe = StableDiffusionPipeline.from_pretrained(
+        model_id, torch_dtype=torch.float16 if device == 'cuda' else torch.float32
+    ).to(device)
+
+    generator = torch.Generator(device=device).manual_seed(seed)
+    image = pipe(PROMPT, generator=generator, num_inference_steps=30).images[0]
+    image.save(out_path)
+    print(f'saved {out_path}')
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print('usage: diffusion_stego.py "message" out.png')
+        sys.exit(1)
+    generate(sys.argv[1], sys.argv[2])
