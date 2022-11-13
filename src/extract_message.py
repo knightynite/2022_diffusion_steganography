3ffb5d3ae0131c752404fdc133917509a8d4a31f
Alcybr <aslahaddin@gmail.com>
2022-11-12 21:08:24 -0600

    extract: brute-force seed search fallback when prompt is partial

--- src/extract_message.py
+++ src/extract_message.py
@@ -28,3 +28,33 @@ def msg_to_seed(msg: str) -> int:
         hashlib.sha256(SALT + msg.encode('utf-8')).digest()[:4], 'big')
 
 
+def image_distance(a: Image.Image, b: Image.Image) -> float:
+    a = np.asarray(a.convert('RGB'), dtype=np.float32)
+    b = np.asarray(b.convert('RGB'), dtype=np.float32)
+    return float(np.mean((a - b) ** 2))
+
+
+def extract(image_path: str, model_id='runwayml/stable-diffusion-v1-5'):
+    target = Image.open(image_path)
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    pipe = StableDiffusionPipeline.from_pretrained(
+        model_id, torch_dtype=torch.float16 if device == 'cuda' else torch.float32
+    ).to(device)
+
+    best = (None, float('inf'))
+    for candidate in DICT:
+        seed = msg_to_seed(candidate)
+        gen = torch.Generator(device=device).manual_seed(seed)
+        img = pipe(PROMPT, generator=gen, num_inference_steps=30).images[0]
+        d = image_distance(img, target)
+        print(f'  {candidate:20s}  MSE = {d:9.2f}')
+        if d < best[1]:
+            best = (candidate, d)
+    print(f'\nbest match: "{best[0]}"')
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('usage: extract_message.py stego.png')
+        sys.exit(1)
+    extract(sys.argv[1])
